7404c7f9f6da420c9df93be0df1ffaf2908af208
rioliver <richard.oliver.mmltd.co.za>
2020-08-20 17:21:03 +0200

    New board object

--- main.cpp
+++ main.cpp
@@ -7,8 +7,14 @@
 
 class Cell {
 public:
+    Cell(int c, int r, int val) {
+        col = c;
+        row = r;
+        value = val;
+    }
     int col;
     int row;
+    int value;
 };
 
 class Shape {
@@ -26,6 +32,29 @@ public:
     int numBLockedCells;
     std::vector<Cell> blockedCells;
     std::vector<Shape> shapes;
+};
+
+
+class Board {
+public:
+    explicit Board(const Input& input) {
+        for (int k = 0; k < input.dimRow; ++k) {
+            std::vector<Cell> bob;
+            for (int i = 0; i < input.dimCol; ++i) {
+                bob.emplace_back(i, k, -1);
+            }
+            boardCells.push_back(bob);
+        }
+
+        for (auto &shape : input.shapes) {
+            for (auto &cell : shape.cells) {
+                boardCells[cell.col][cell.row].value = cell.value;
+            }
+        }
+
+    }
+
+    std::vector<std::vector<Cell>> boardCells;
 };
 
 class Output {
@@ -53,31 +82,51 @@ int main() {
 
 
 //count number of filled cells
-int numberOfFilledCells(const std::vector<Shape>& shapes) {
+int numberOfFilledCells(const std::vector<Shape> &shapes) {
     int numberOfFilledCells = 0;
-    for (auto & shape : shapes) {
+    for (auto &shape : shapes) {
         numberOfFilledCells += shape.cells.size();
     }
 
     return numberOfFilledCells;
 }
 
+
+bool isNewWhiteCell(int i, int j, Board board, std::vector<Cell> foundCells) {
+    return true;
+}
+
 //count number of individual empty groups
-int numberOfEmptyGroupings(const std::vector<Shape>& shapes) {
-    int numberOfFilledCells = 0;
-    for (auto & shape : shapes) {
-        numberOfFilledCells += shape.cells.size();
+int numberOfEmptyGroupings(const Input &input) {
+    Board board = Board(input);
+
+    std::vector<Cell> foundCells;
+
+    for (int i = 0; i < input.dimCol; ++i) {
+        for (int j = 0; j < input.dimRow; ++j) {
+            if(board.boardCells[i][j].value == -1) {
+                if(isNewWhiteCell(i, j, board, foundCells)){
+                    foundCells.push_back(board.boardCells[i][j]);
+                }
+            }
+        }
     }
+
+    return foundCells.size();
+
 }
 
-int numberOfSoloWhiteBlocks(const std::vector<Shape>& shapes) {
+
+
+
+int numberOfSoloWhiteBlocks(const std::vector<Shape> &shapes) {
 
 }
 
-int calculateScore(Input input) {
+int calculateScore(const Input &input) {
     int totalCapacity = input.dimCol * input.dimRow;
     int score = numberOfFilledCells(input.shapes) * 10;
-    score -= numberOfEmptyGroupings(input.shapes) * 2;
+    score -= numberOfEmptyGroupings(input) * 2;
     score -= numberOfSoloWhiteBlocks(input.shapes) * 4;
 
     return floor(totalCapacity / numberOfFilledCells(input.shapes) * score);
