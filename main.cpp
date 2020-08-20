c732ea7ee08fd4d91f473d572c81551d3f9b618a
Tiaan Malherbe <tiaan@tiaan.net>
2020-08-20 16:35:15 +0200

    classesss

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+//#include "ortools/linear_solver/linear_solver.h"
+
+class Cell {
+    int x;
+    int y;
+};
+
+class Cells {
+    Cell cells[];
+};
+
+class Shape {
+public:
+    int id;
+    int numAvailable;
+    Cells cells;
+};
+class Input {
+public:
+    int dimX;
+    int dimY;
+    int numShapes;
+    int numBLockedCells;
+    Cells blockedCells;
+    Shape shapes[];
+};
+
+class Output {
+    Shape shapes[];
+};
+
+
 
 int main() {
+//    operations_research::SimpleLpProgram();
+
     std::cout << "Hello, World!" << std::endl;
     std::ifstream file("input.txt");
     std::string str;
